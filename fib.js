cd08b452866f80f84764a4c605358eb874205cb0
Jeremy Winnett <winnettjeremy@gmail.com>
2026-06-04 17:43:05 -0400

    fibonacci sequences

--- fib.js
+++ fib.js
@@ -1,42 +1,25 @@
-function fibs(num) {
-  fibSeq = [];
-  let num1 = 0;
-  let num2 = 1;
-  let sum;
-  if (num === 1) {
-    return num1;
-  } else if (num === 2) {
-    return num2;
-  } else {
-    for (let i = 3; i <= num; i++) {
-      console.log(num1, num2, i)
-      sum = num1 + num2;
-      num1 = num2;
-      num2 = sum;
-    }
-    //console.log(num, num1, num2, sum)
-    return num2;
+#!/usr/bin/env node
+
+
+function fibs(n) {
+  fibSeq = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fibSeq.push(fibSeq[i - 1] + fibSeq[i  - 2]);
   }
+  return fibSeq;
 }
-
-console.log("Fibonacci(8): " + fibs(8));
-console.log("Fibonacci(8): " + fibs(29));
+console.log("Fibonacci(8): " + JSON.stringify(fibs(8)));
 
 
-
-
-
-
-
-
-// function fibonacci(n) {
-//   if(n < 2) {
-//     return [n];
-//   } else {
-//     return ([fibonacci(n-1)+fibonacci(n-2)]);
-//   }
-// }
-
-// //console.log(fibonacci([0, 1, 1, 2, 3, 5, 8, 13]))
-
-// console.log(fibonacci(8))
+function fibsRec(n) {
+  if(n == 1) {
+    return [0];
+  }
+  if (n == 2) {
+    return [0, 1];
+  }
+  const prev = fibsRec(n - 1);
+  prev.push(prev.at(-1) + prev.at(-2));
+  return prev;
+}
+console.log("Fibs(8): " + JSON.stringify(fibsRec(8)));
